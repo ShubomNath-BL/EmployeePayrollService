44c11993c4ab2a3a182cd75c9257938322a764ec
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-07-19 15:33:53 +0530

    Added: welcome message added

---
+++ src/main/java/com/employeepayroll/EmployeePayrollServices.java
@@ -0,0 +1,7 @@
+package com.employeepayroll;
+
+public class EmployeePayrollServices {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee payroll services");
+    }
+}
