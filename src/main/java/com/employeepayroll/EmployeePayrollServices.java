bb0d5c0438abe6640f42463ec9b6bf792fbaa18d
ShubomNath-BL <shubhom.nath@bridgelabz.com>
2022-07-20 23:24:49 +0530

    Added: Count file functionality added

--- src/main/java/com/employeepayroll/EmployeePayrollServices.java
+++ src/main/java/com/employeepayroll/EmployeePayrollServices.java
@@ -1,7 +1,6 @@
 package com.employeepayroll;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -28,7 +27,6 @@ public class EmployeePayrollServices {
         System.out.println("Welcome to Employee payroll services");
         ArrayList<EmployeePayrollData> employeePayrollDataList = new ArrayList<>();
         EmployeePayrollServices employeePayrollServices=new EmployeePayrollServices(employeePayrollDataList);
-        Scanner ConsoleInputReader = new Scanner(System.in);
         employeePayrollServices.operations();
     }
 
@@ -37,13 +35,16 @@ public class EmployeePayrollServices {
         boolean change=true;
         do{
             System.out.println("\n Choose operations:-");
-            System.out.println("1.Read & write\n2.Exit");
+            System.out.println("1.Read & write\n2.Content count\n3.Exit");
             switch (ConsoleInputReader.nextInt()){
                 case 1:
                     readEmployeePayrollData(ConsoleInputReader);
                     writeEmployeePayrollData();
                     break;
                 case 2:
+                    countContent();
+                    break;
+                case 3:
                     change=false;
                     System.out.println("Exiting");
             }
@@ -70,5 +71,33 @@ public class EmployeePayrollServices {
             throw new RuntimeException(e);
         }
         //System.out.println("\n Writing employee payroll roaster to console: "+ employeePayrollDataList);
+    }
+    public void countContent(){
+        File file = new File("C:\\Users\\Lenovo\\IdeaProjects\\EmployeePayrollService\\src\\test.txt");
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        int wordCount = 0;
+        int lineCount = 0;
+        try{
+            while ((line = bufferedReader.readLine()) != null) {
+                if (line.equals("")) {
+                    lineCount += 1;
+                }else {
+                    String words[] = line.split("\\s+");
+                    wordCount += words.length;
+                }lineCount++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Numbers of line: "+ lineCount);
+        System.out.println("Numbers of words: "+ wordCount);
     }
 }
